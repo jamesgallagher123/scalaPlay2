1b8dfaf001e5f35ec04a2de1c9a9c097306f80d1
jamesgallagher123 <james.gallagher@qa.com>
2017-07-26 16:32:07 +0100

    create and view

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,64 @@
 package controllers
 
-import play.api._
+import javax.inject.Inject
+
+import models.Item
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 
-class Application extends Controller {
+import scala.concurrent.Future
+
+class Application @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def listItems = Action { implicit request =>
+    Ok(views.html.listItems(Item.items, Item.createItemForm))
+  }
+
+  def createItem = Action { implicit request =>
+
+    val formValidationResult = Item.createItemForm.bindFromRequest
+    formValidationResult.fold({ formWithErrors =>
+      BadRequest(views.html.listItems(Item.items, formWithErrors))
+    }, { item =>
+      Item.items.append(item)
+      Redirect(routes.Application.listItems)
+    })
+
+  }
+
+  def loadDeleteItem = Action { implicit request =>
+    Ok(views.html.deleteItem(Item.items, Item.deleteItemForm))
+  }
+
+  def deleteItem = Action{ implicit request =>
+
+
+
+    Redirect(routes.Application.index())
+  }
+
+
+
+  //upload image test
+  def viewUpload = Action {
+    Ok(views.html.imageUpload())
+  }
+
+  def upload = Action(parse.multipartFormData) { request =>
+    request.body.file("picture").map { picture =>
+      import java.io.File
+      val filename = picture.filename
+      val contentType = picture.contentType
+      picture.ref.moveTo(new File(s"C:/Users/Administrator/Desktop/pics/$filename"))
+      Ok("File uploaded")
+    }.getOrElse {
+      Redirect(routes.Application.index).flashing(
+        "error" -> "Missing file")
+    }
   }
 
 }
---
+++ app/models/Item.scala
@@ -0,0 +1,39 @@
+package models
+
+import java.awt.image.BufferedImage
+
+import play.api.data._
+import play.api.data.Forms._
+
+import scala.collection.mutable.ArrayBuffer
+
+case class Item(name: String, description: String,
+                maker: String, warranty: Int,
+                price: Int, discount: Int,
+                seller: String)
+
+object Item {
+
+  val createItemForm = Form(
+    mapping(
+      "name" -> nonEmptyText,
+      "description" -> nonEmptyText,
+      "maker" -> nonEmptyText,
+      "warranty" -> number(min = 0, max = 5),
+      "price" -> number(min = 0, max = 100),
+      "discount" -> number,
+      "seller" -> nonEmptyText
+    )(Item.apply)(Item.unapply)
+  )
+
+  val deleteItemForm = Form(
+      "select" -> Forms.list(nonEmptyText)
+  )
+
+  val items = ArrayBuffer(
+    Item("name1", "description1", "maker1", 1, 2, 0, "seller1"),
+    Item("name2", "description2", "maker2", 1, 2, 0, "seller2"),
+    Item("name3", "description3", "maker3", 1, 2, 0, "seller3")
+  )
+
+}
